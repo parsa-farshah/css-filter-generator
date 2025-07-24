4bb3a3f33a408965d43b2582de47603844c9a9fe
parsa <parsafarashah@gmail.com>
2025-07-25 03:03:10 +0330

    finish

--- assets/js/master.js
+++ assets/js/master.js
@@ -28,9 +28,46 @@ let txtHue = document.getElementById("numbHue");
 let hueTxt = document.getElementById("hueTxt");
 let currentHue = 0;
 
+// ------------------------ invert ----------------
+let inpInvert = document.getElementById("inpInvert");
+let txtInvert = document.getElementById("numbInvert");
+let invertTxt = document.getElementById("invertTxt");
+let currentInvert = 0;
+
+// ------------------------ opacity ----------------
+let inpOpacity = document.getElementById("inpOpacity");
+let txtOpacity = document.getElementById("numbOpacity");
+let opacityTxt = document.getElementById("opacityTxt");
+let currentOpacity = 100;
+
+// ------------------------ Saturate ----------------
+let inpSaturate = document.getElementById("inpSaturate");
+let txtSaturate = document.getElementById("numbSaturate");
+let saturateTxt = document.getElementById("saturateTxt");
+let currentSaturate = 100;
+
+// ------------------------ Sepia ----------------
+let inpSepia = document.getElementById("inpSepia");
+let txtSepia = document.getElementById("numbSepia");
+let sepiaTxt = document.getElementById("sepiaTxt");
+let currentSepia = 0;
+
+// copy
+var copyText = document.getElementById("myInput");
+let copyButton = document.getElementById("copyButton");
+
+// creator
+let creatorBt = document.getElementById("creatorBt");
+let creator = document.getElementById("creator");
+
+// closeBtn
+let closeBtn = document.getElementById("closeBtn");
+
 // update filters
 function updateFilters() {
-  image.style.filter = `blur(${currentBlur}px)  brightness(${currentBrighness}%) contrast(${currentContrast}%) grayscale(${currentGrayscale}%) hue-rotate(${currentHue}deg) `;
+  image.style.filter = `blur(${currentBlur}px)  brightness(${currentBrighness}%) contrast(${currentContrast}%) grayscale(${currentGrayscale}%) hue-rotate(${currentHue}deg) 
+  invert(${currentInvert}%) opacity(${currentOpacity}%) saturate(${currentSaturate}%) sepia(${currentSepia}%)
+  `;
 }
 
 //------------------------ blur radius event ------------------
@@ -84,4 +121,70 @@ inpHue.addEventListener("change", () => {
   currentHue == 0 ? " " : (hueTxt.innerHTML = `hue-rotate(${currentHue}deg)`);
 
   updateFilters();
+});
+
+// ------------------------ invert event ----------------
+
+inpInvert.addEventListener("change", () => {
+  currentInvert = inpInvert.value;
+  txtInvert.textContent = currentInvert;
+  currentInvert == 0
+    ? " "
+    : (invertTxt.innerHTML = `invert(${currentInvert}%)`);
+
+  updateFilters();
+});
+
+// ------------------------ opacity event ----------------
+
+inpOpacity.addEventListener("change", () => {
+  currentOpacity = inpOpacity.value;
+  txtOpacity.textContent = currentOpacity;
+  currentOpacity == 0
+    ? " "
+    : (opacityTxt.innerHTML = `opacity(${currentOpacity}%)`);
+
+  updateFilters();
+});
+
+// ------------------------ Saturate event ----------------
+
+inpSaturate.addEventListener("change", () => {
+  currentSaturate = inpSaturate.value;
+  txtSaturate.textContent = currentSaturate;
+  currentSaturate == 0
+    ? " "
+    : (saturateTxt.innerHTML = `opacity(${currentSaturate}%)`);
+
+  updateFilters();
+});
+
+// ------------------------ Sepia event ----------------
+
+inpSepia.addEventListener("change", () => {
+  currentSepia = inpSepia.value;
+  txtSepia.textContent = currentSepia;
+  currentSepia == 0 ? " " : (sepiaTxt.innerHTML = `opacity(${currentSepia}%)`);
+
+  updateFilters();
+});
+
+// copy
+copyButton.addEventListener("click", () => {
+  const textToCopy = copyText.innerText.trim();
+
+  navigator.clipboard
+    .writeText(textToCopy)
+    .then(() => alert("کپی شد:\n" + textToCopy))
+    .catch((err) => console.error("خطا در کپی:", err));
+});
+
+// Creator button
+creatorBt.addEventListener("click", () => {
+  creator.classList.add("creatorOpen");
+});
+
+// close btn
+closeBtn.addEventListener("click", () => {
+  creator.classList.remove("creatorOpen");
 });
